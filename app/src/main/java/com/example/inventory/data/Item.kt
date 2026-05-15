d8fac5c478a0d628c36645fbcbf70bd16ffd774a
wwayard <namenastua@gmail.com>
2026-05-16 01:15:13 +0300

    Создан Entity-класс Item

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -15,12 +15,17 @@
  */
 
 package com.example.inventory.data
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
