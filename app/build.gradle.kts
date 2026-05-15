ddb4faf946aa79ff133c73d646b85f43dcd8e189
wwayard <namenastua@gmail.com>
2026-05-16 01:05:54 +0300

    Добавлены зависимости библиотеки Room в build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,7 @@ dependencies {
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
